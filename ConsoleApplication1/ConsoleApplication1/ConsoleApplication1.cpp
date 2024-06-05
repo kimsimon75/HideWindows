fde52cb301d91750b0f34f6ad3ffb727c4c526e3
kimsimon75 <79342840+kimsimon75@users.noreply.github.com>
2024-06-05 14:56:34 +0900

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,139 @@
+﻿#include <Windows.h>
+#include "후킹.h"
+#include <thread>
+
+//아이콘 출처 : <a href="https://www.flaticon.com/kr/free-icons/-" title="양자 컴퓨팅 아이콘">양자 컴퓨팅 아이콘 제작자: xnimrodx - Flaticon</a>
+
+using namespace std;
+
+template <class T>
+
+void SafeRelease(T** ppT) {
+
+	if (*ppT) {
+
+		(*ppT)->Release();
+
+		*ppT = NULL;
+
+	}
+
+}
+
+void CheckPID()
+{
+	while (1)
+	{
+		CoInitialize(NULL);
+
+
+
+		IMMDeviceEnumerator* pEnumerator = NULL;
+
+		CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, IID_PPV_ARGS(&pEnumerator));
+
+
+
+		IMMDevice* pDevice = NULL;
+
+		pEnumerator->GetDefaultAudioEndpoint(eRender, eMultimedia, &pDevice);
+
+
+
+		IAudioSessionManager2* pSessionManager = NULL;
+
+		pDevice->Activate(__uuidof(IAudioSessionManager2), CLSCTX_ALL, NULL, (void**)&pSessionManager);
+
+
+
+		IAudioSessionEnumerator* pSessionList = NULL;
+
+		pSessionManager->GetSessionEnumerator(&pSessionList);
+
+
+
+		int sessionCount = -1;
+
+		pSessionList->GetCount(&sessionCount);
+
+
+
+		for (int i = 0; i < sessionCount; i++) {
+
+			IAudioSessionControl* pSessionControl = NULL;
+
+			pSessionList->GetSession(i, &pSessionControl);
+
+
+
+			IAudioSessionControl2* pSessionControl2 = NULL;
+
+			HRESULT hr = pSessionControl->QueryInterface(__uuidof(IAudioSessionControl2), (void**)&pSessionControl2);
+
+			if (SUCCEEDED(hr)) {
+
+				DWORD processId = -1;
+
+				pSessionControl2->GetProcessId(&processId);
+
+				std::wstring processName = GetProcessNameByPID(processId);
+
+
+
+				// 여기서 할 일 하면 됨
+
+				std::wcout << L"PID: " << processId << ", Name: " << processName << std::endl;
+
+				if (m.find(processName) == m.end())
+				{
+					m.insert(pair<wstring, DWORD>(processName, processId));
+				}
+
+				pSessionControl2->Release();
+
+			}
+
+			pSessionControl->Release();
+
+		}
+
+
+
+		SafeRelease(&pSessionList);
+
+		SafeRelease(&pSessionManager);
+
+		SafeRelease(&pDevice);
+
+		SafeRelease(&pEnumerator);
+
+
+
+		CoUninitialize();
+		Sleep(100);
+	}
+}
+
+int main()
+{
+	thread t1(CheckPID);
+
+	HINSTANCE hInstance = GetModuleHandle(NULL);
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, hInstance, 0);
+
+	mouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseProc, hInstance, 0);
+	if (mouseHook == NULL) {
+		std::cerr << "Failed to install mouse hook!" << std::endl;
+		return 1;
+	}
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+	}
+	UnhookWindowsHookEx(keyboardHook);
+	UnhookWindowsHookEx(mouseHook);
+}
+  
+
+
---
+++ ConsoleApplication1/ConsoleApplication1/후킹.h
@@ -0,0 +1,74 @@
+#pragma once
+#include <Windows.h>>
+#include "숨기기, 보이기.h"
+
+HHOOK keyboardHook;
+HHOOK mouseHook;
+
+LRESULT __stdcall MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode >= 0)
+	{
+
+		MSLLHOOKSTRUCT* hookStruct = (MSLLHOOKSTRUCT*)lParam;
+
+			if (wParam == WM_XBUTTONDOWN)
+			{
+				
+				return 1;
+			}
+			if (wParam == WM_XBUTTONUP)
+			{
+				if (hookStruct->mouseData == 0x20000)
+				{
+					HideWindow();
+				}
+				else if (hookStruct->mouseData == 0x10000)
+				{
+					ShowWindow();
+				}
+				return 1;
+			}
+		
+		
+	}
+	return CallNextHookEx(mouseHook, nCode, wParam, lParam); // 이전으로 돌아가도록 변경
+}
+
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode >= 0)
+	{
+		KBDLLHOOKSTRUCT* kbStruct = (KBDLLHOOKSTRUCT*)lParam;
+
+		if (wParam == WM_KEYDOWN)
+		{
+			// F1 키를 누른 경우
+			if (kbStruct->vkCode == VK_F1)
+			{
+				ShowWindowAll();
+				return 1;
+			}
+			else if (kbStruct->vkCode == VK_ESCAPE && GetAsyncKeyState(VK_CONTROL) & 0x8000) 
+			{
+				ShowWindow(GetConsoleWindow(), SW_SHOW);
+				ShowWindowAll();
+				UnhookWindowsHookEx(keyboardHook);
+				UnhookWindowsHookEx(mouseHook);
+				PostMessage(GetConsoleWindow(), WM_CLOSE, NULL, NULL);
+				return 1;
+			}
+			else if (kbStruct->vkCode == VK_F2)
+			{
+				HideWindow();
+				return 1;
+			}
+		}
+		else if (wParam == WM_KEYUP && kbStruct->vkCode == VK_F1)
+		{
+			return 1;
+		}
+	}
+	// 후킹된 이벤트 처리를 계속합니다.
+	return CallNextHookEx(keyboardHook, nCode, wParam, lParam);
+}
